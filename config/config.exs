7f57465582aac202a0986e86f2aa839b6fddde73
neven <nevendrean@yahoo.fr>
2022-08-15 10:37:28 +0200

    cleaned

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,5 @@
 import Config
 
-config :my_app, Movies.Repo,
-  database: "my_app_repo",
-  username: "user",
-  password: "pass",
-  hostname: "localhost"
-
-# config :my_app, MyApp.Repo,
-#   database: "my_app_repo",
-#   username: "postgres",
-#   password: "pass",
-#   hostname: "localhost",
-#   migration_timestamps: [type: :utc_datetime]
 
 config :my_app,
   ecto_repos: [MyApp.Repo]
--- config/test.exs
+++ config/test.exs
@@ -3,11 +3,7 @@ import Config
 config :my_app, MyApp.Repo,
   database: "my_app_repo",
   username: "postgres",
-  password: "pass",
+  password: "postgres",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
   migration_timestamps: [type: :utc_datetime]
-
-# ownership_timeout: 999_999
-
-# Postgres connection timeout => ownership_timeout
